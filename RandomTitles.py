3611b47db8eafcb468b7b9a6c5a93758298ea900
RandomVoat <randomvoat@gmail.com>
2016-01-20 23:27:11 -0500

    Updated/improved - now posts 1 title with link to thread

--- RandomTitles.py
+++ RandomTitles.py
@@ -1,39 +1,29 @@
 # Returns random post titles to RandomVoat.py
 
 import random
+import subprocess
 
+FILE = [line.rstrip('\n') for line in open('/home/pi/RandomVoat/postTitles_revise.txt')]
+EVEN = [0,2,4,6,8,10,12,14,16,18,20]
+CMD = "ruby /home/pi/RandomVoat/scraper_voat_revise.rb"
 
-# Grab 50 random lines from scraper dump file
-sentencePool = random.sample(list(line.rstrip('\n') for line in open('/home/pi/RandomVoat/postTitles.txt')), 50)
-                                                                     
 def main():
 
-    run = True
+    randomSelect = random.choice(EVEN)
+    nextLine = randomSelect + 1
     
-    while run:
-        totalPost = [random.choice(sentencePool),
-                     random.choice(sentencePool),
-                     random.choice(sentencePool)]
-        
-        # check for title duplications
-        if compareChoices(totalPost):
-            joinedPost = ' '.join(totalPost)
-            charCount = len(joinedPost)
+    subprocess.call(CMD, shell=True)
 
-        # check for twitter's 140 char max
-        if charCount <= 140:
-            return joinedPost
-            run = False
+    title = FILE[randomSelect]
+    href = FILE[nextLine]
+    
+    if len(title) <= 110:
+        status = (title + ' - ' + href)
+    else:
+        status = (title[:110] + '... - ' + href)
 
-def compareChoices(post):
+    return status
 
-    noDuplicates = False
 
-    for i in range(1,len(post)):
-        if post[i] != post[i-1]:
-            noDuplicates = True
-
-    return noDuplicates
-
-	    
 main()
+
